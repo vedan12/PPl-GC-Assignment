651e62fa80dad3eebd23cb63c4cc1d3fcf6b715c
vedan12 <156008040+vedan12@users.noreply.github.com>
2024-03-15 12:14:11 +0530

    counting word and chars in array

---
+++ counting_word_and_chars_in_array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+	int l,i,w=0,c=0;
+	char statement[100];
+	printf("Enter the length of sentence : ");
+	scanf("%d",&l);
+	printf("Enter the sentence : ");
+	for( i=0; i<l;i++){
+	scanf("%c",&statement[i]);
+	}
+	for(i=0;i<l;i++){
+		if((int)statement[i]==32){
+			w++;
+		}
+		else{
+			continue;
+		}
+	}
+	for(i=0;i<l;i++){
+		if((int)statement[i]==32){
+			continue;
+		}
+		else{
+			c++;
+		}
+	}
+	printf("There are %d words , %d letters and %d spaces in above sentence",w+1,l-w+1,w);
+	return 0;
+}
+	
